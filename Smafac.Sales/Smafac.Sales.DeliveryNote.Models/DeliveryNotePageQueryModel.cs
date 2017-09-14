4aaf85e66fb2a0cd9aac35eb52f1a559bebf92ab
liming <lovetioner@163.com>
2017-09-14 17:00:56 +0800

    修改了基本框架和订单样式

--- Smafac.Sales/Smafac.Sales.DeliveryNote.Models/DeliveryNotePageQueryModel.cs
+++ Smafac.Sales/Smafac.Sales.DeliveryNote.Models/DeliveryNotePageQueryModel.cs
@@ -11,10 +11,11 @@ namespace Smafac.Sales.DeliveryNote.Models
     public class DeliveryNotePageQueryModel : DateSpanPageQueryModel
     {
         [Display(Name = "客户")]
+        [QueryProperty]
         public Guid CustomerId { get; set; }
         [Display(Name = "分类")]
+        [QueryProperty]
         public Guid CategoryId { get; set; }
-
         protected override string DateColumn => "DeliveryTime";
     }
 }
